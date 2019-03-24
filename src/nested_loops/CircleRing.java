5427351a4b2c2549ce865d713abfe675334751dd
uevan2 <league@WTS14-2.attlocal.net>
2019-03-24 11:25:42 -0700

    level 0 mod 5
    almost done :)

--- src/nested_loops/CircleRing.java
+++ src/nested_loops/CircleRing.java
@@ -10,26 +10,31 @@ public class CircleRing {
     public static void main(String[] args) throws Exception {
 
         // 1. Make a new Robot
-
+    		Robot rob = new Robot();
 
        //  2. Set your robot’s position to x=150 and y=200
-
-        // 3. Put the robot's pen down
-
+    		rob.setX(150);
+    		rob.setX(200);
+    		// 3. Put the robot's pen down
+    		rob.penDown();
         // 4. Set the robot’s speed to 10
-
+    		rob.setSpeed(100);
         // 5. Do everything below here 360 times (use i as the counter)
-
+    		for(int i = 0; i < 360; i++) {
 	        // 6. Move the robot 3 pixels
-
+    				rob.move(3);
        //  7. Turn the robot 1 degree
-
+    				rob.turn(1);
         // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
-
+    				if(i%20 == 0) {
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
-
+    					for(int j = 0; j < 360; j++) {
 				// 10. Move the robot 1 pixel
-
+    						rob.move(1);
 				// 11. Turn the robot 1 degree
-}
+    						rob.turn(1);
+    				}
+    			}
+    		}
+    	}
 }
---
+++ src/nested_loops/LoopGauntlet.java
@@ -0,0 +1,124 @@
+package nested_loops;
+
+import javax.swing.JOptionPane;
+
+public class LoopGauntlet {
+	public static void main(String[] args) {
+		
+		//new loop 1
+		String printA = "1. \n";
+		for(int a = 0; a<=100; a++) {
+			printA = printA + ", "+a;
+			if(a%5==0) {
+				printA = printA + "\n";
+			}
+		}
+		JOptionPane.showMessageDialog(null, printA);
+		
+		//new loop 2
+		String printB = "2. \n";
+		for(int b = 100; b>=0; b--) {
+			printB = printB + ", "+b;
+			if(b%5==0) {
+				printB = printB + "\n";
+			}
+		}
+		JOptionPane.showMessageDialog(null, printB);
+		
+		//new loop 3
+		String printC = "3. \n";
+		for(int c = 2; c<=100; c = c+2) {
+			printC = printC + ", "+c;
+			if(c%5==0) {
+				printC = printC + "\n";
+			}
+		}
+		JOptionPane.showMessageDialog(null, printC);
+		
+		//new loop 4
+		String printD = "4. \n";
+		for(int d = 1; d<=99; d = d+2) {
+			printD = printD + ", "+d;
+			if(d%5==0) {
+				printD = printD + "\n";
+			}
+		}
+		JOptionPane.showMessageDialog(null, printD);
+		
+		//new loop 5
+		String printE = "5. \n";
+		for(int e = 0; e<=500; e++) {
+			
+			if(e%2==0) {
+				printE = printE + ", "+e+" is even";
+			}
+			
+			if(e%2!=0) {
+				printE = printE + ", "+e+" is odd";
+			}
+			
+			if(e%12==0) {
+				printE = printE + "\n";
+			}
+		}
+		JOptionPane.showMessageDialog(null, printE);
+		
+		//new loop 6
+		String printF = "6. \n";
+		for(int f = 0; f<=777; f = f +7) {
+			printF = printF + ", "+f;
+			if(f%5==0) {
+				printF = printF + "\n";
+			}
+		}
+		JOptionPane.showMessageDialog(null, printF);
+		
+		//new loop 7
+				String printG = "7. \n";
+				int counter =0;
+				for(int g = 2004; g<=2018; g++) {
+					
+					printG = printG + "in "+g+" I was "+counter+" years old \n";
+					
+					counter++;
+				
+				}				
+				printG = printG +"In 2019 I'm turning 15 years old";
+				JOptionPane.showMessageDialog(null, printG);
+		//new loop 8
+		String printHI = "Nested For-Loops \n1. \n";
+		for(int h = 0; h<=2; h++) {
+			for(int i = 0; i<=2; i++) {
+				
+			printHI = printHI + h+" "+i+"\n";
+			}
+
+		}
+		JOptionPane.showMessageDialog(null, printHI);
+	
+		//new loop 9
+				String printJ = "2. \n";
+				for(int j = 1; j<=9; j++) {
+					printJ = printJ + j+" ";
+					if(j%3==0) {
+						printJ = printJ + "\n";
+					}
+				}
+				JOptionPane.showMessageDialog(null, printJ);
+				
+				//new loop 8
+				String printLM = "3. \n";
+				for(int l = 1; l<=10; l++) {
+					for(int m = 1; m<=10; m++) {
+						
+						printLM = printLM + l+" "+m;
+					}
+					printLM = printLM +"\n";
+				}
+				JOptionPane.showMessageDialog(null, printLM);
+	
+	
+	
+	
+	}
+}
